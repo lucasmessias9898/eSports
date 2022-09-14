c9268b89f32ab9bbf70a7dc7db1b0ada0b98a901
lucasmessias9898 <36428045+lucasmessias9898@users.noreply.github.com>
2022-09-14 12:33:16 -0300

    alterações na LP

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,4 @@
+import { MagnifyingGlassPlus } from 'phosphor-react'
 import './styles/main.css'
 
 import logo from './assets/Logo.svg'
@@ -18,24 +19,44 @@ function App() {
         </h1>
 
         <div className="grid grid-cols-6 gap-6 mt-16">
-            <a href="">
+            <a href="" className="relative rounded-lg overflow-hidden">
                 <img src={fortnite} alt="" />
+                <div className="w-full mt-16 pb-4 px-4 bg-game-grandient absolute bottom-0 left-0 right-0">
+                    <strong className='font-bold text-white block'>Fortnite</strong>
+                    <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+                </div>
             </a>
-            <a href="">
+            <a href="" className="relative">
                 <img src={apex} alt="" />
             </a>
-            <a href="">
+            <a href="" className="relative">
                 <img src={lol} alt="" />
             </a>
-            <a href="">
+            <a href="" className="relative">
                 <img src={dota2} alt="" />
             </a>
-            <a href="">
+            <a href="" className="relative">
                 <img src={wwc} alt="" />
             </a>
-            <a href="">
+            <a href="" className="relative">
                 <img src={csGo} alt="" />
             </a>
+        </div>
+
+        <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden'>
+            <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+                <div>
+                    <strong className='text-2xl text-white font-black block'>Não encrontrou seu duo</strong>
+                    <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
+                </div>
+
+                <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded
+                    flex items-center gap-3'
+                >
+                    <MagnifyingGlassPlus size={24}/>
+                    Publicar anúncio
+                </button>
+            </div>
         </div>
     </div>
   )
